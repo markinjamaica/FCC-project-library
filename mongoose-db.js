ff917c1fa654a0bf89832cb247fc371faf370288
Mark Gauffin <markgauffin@gmail.com>
2022-01-31 04:25:41 -0500

    add book schema and model

--- mongoose-db.js
+++ mongoose-db.js
@@ -7,24 +7,14 @@ module.exports = () => {
     // Define schemas
     const { Schema, model } = mongoose;
 
-    const issueSchema = new Schema({
-        project_name: String,
-        issue_title: String,
-        issue_text: String,
-        created_on: Date,
-        updated_on: Date,
-        created_by: String,
-        assigned_to: String,
-        open: Boolean,
-        status_text: String,
+    const bookSchema = new Schema({
+        book_title: String,
+        comments: [String],
     });
 
     // Define models
     // const Project = model('Project', projectSchema);
-    const Issue = model('Issue', issueSchema);
+    const Book = model('Book', bookSchema);
 
-    return Issue;
+    return Book;
 };
-
-
-
